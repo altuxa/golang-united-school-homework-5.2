d16e6b52cec4c73c285c83f36c185f477d96354e
Altynbek <altuxadark98@gmail.com>
2022-05-20 16:07:48 +0600

    alfa

--- cache.go
+++ cache.go
@@ -3,21 +3,28 @@ package cache
 import "time"
 
 type Cache struct {
+	data map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		data: make(map[string]string),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	if data, ok := c.data[key]; ok {
+		return data, true
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 }
